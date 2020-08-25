0155543d9292136d99e75a938535051a41e7c03c
Dhamale, Sandeep <sandeep.dhamale@willis.com>
2020-08-25 12:39:45 +0530

    Initial Commit for Lists

---
+++ TestLists/ListsTests.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestAutomation.Framework.Helpers.Strings;
+
+namespace TestLists
+{
+    [TestClass]
+    public class ListsTests
+    {
+        #region sequence
+
+        //Pass
+        [TestMethod]
+        public void TestSequence_SequenceTrue_ExactMatchTrue_IgnoreCaseTrue()
+        {
+            List<string> listActualValues = new List<string> {"abc", "def", "pqr", "uvw", "xyz" };
+            string listExpectedValues = "abc;def;pqr;uvw;xyz";
+            bool areEqual = ListExtentions.VerifyListValues(listActualValues, listExpectedValues, false, true, false, false);
+            Assert.AreEqual(true, areEqual);
+        }
+
+        //Fail
+        [TestMethod]
+        public void TestSequence_SequenceFalse_ExactMatchTrue_IgnoreCaseTrue()
+        {
+            List<string> listActualValues = new List<string> { "abc", "def", "pqr", "uvw", "xyz" };
+            string listExpectedValues = "abc;pqr;uvw;xyz;def";
+            bool areEqual = ListExtentions.VerifyListValues(listActualValues, listExpectedValues, false, true, false, false);
+            Assert.AreEqual(true, areEqual);
+        }
+
+        #endregion
+
+        #region IgnoreCase
+
+        //Pass
+        [TestMethod]
+        public void TestIgnoreCase_SequenceTrue_ExactMatchTrue_IgnoreCaseTrue()
+        {
+            List<string> listActualValues = new List<string> { "abc", "def", "pqr", "UVW", "xyz" };
+            string listExpectedValues = "abc;def;pqr;uvw;xyz";
+            bool areEqual = ListExtentions.VerifyListValues(listActualValues, listExpectedValues, false, true, false, true);
+            Assert.AreEqual(true, areEqual);
+        }
+
+        //Fail
+        [TestMethod]
+        public void TestIgnoreCase_SequenceTrue_ExactMatchTrue_IgnoreCaseFalse()
+        {
+            List<string> listActualValues = new List<string> { "abc", "def", "pqr", "UVW", "xyz" };
+            string listExpectedValues = "abc;def;pqr;uvw;xyz";
+            bool areEqual = ListExtentions.VerifyListValues(listActualValues, listExpectedValues, false, true, false, false);
+            Assert.AreEqual(true, areEqual);
+        }
+
+        #endregion
+
+        #region Exactmatch
+
+        //Pass
+        [TestMethod]
+        public void TestExactmatch_SequenceTrue_ExactMatchTrue_IgnoreCaseTrue()
+        {
+            List<string> listActualValues = new List<string> { "abc", "def", "pqr", "uvw", "xyz" };
+            string listExpectedValues = "abc;def;pqr;uvw;xyz";
+            bool areEqual = ListExtentions.VerifyListValues(listActualValues, listExpectedValues, false, true, false, true);
+            Assert.AreEqual(true, areEqual);
+        }
+
+        //Fail
+        [TestMethod]
+        public void TestExactmatch_SequenceTrue_ExactMatchFalse_IgnoreCaseTrue()
+        {
+            List<string> listActualValues = new List<string> { "abc", "defg", "pqr", "uvw", "xyz" };
+            string listExpectedValues = "abc;def;pqr;uvw;xyz";
+            bool areEqual = ListExtentions.VerifyListValues(listActualValues, listExpectedValues, false, true, false, false);
+            Assert.AreEqual(true, areEqual);
+        }
+
+        #endregion
+
+        #region SingleData
+
+        //Pass
+        [TestMethod]
+        public void TestIsSingleData_SequenceTrue_ExactMatchTrue_IgnoreCaseTrue()
+        {
+            List<string> listActualValues = new List<string> { "abc", "abc", "abc", "abc", "abc" };
+            string listExpectedValues = "abc";
+            bool areEqual = ListExtentions.VerifyListValues(listActualValues, listExpectedValues, true, true, false, true);
+            Assert.AreEqual(true, areEqual);
+        }
+
+        //Fail
+        [TestMethod]
+        public void TestIsSingleData_SequenceTrue_ExactMatchFalse_IgnoreCaseTrue()
+        {
+            List<string> listActualValues = new List<string> { "abc", "abc", "pqr", "abc", "abc" };
+            string listExpectedValues = "abc";
+            bool areEqual = ListExtentions.VerifyListValues(listActualValues, listExpectedValues, true, true, false, false);
+            Assert.AreEqual(true, areEqual);
+        }
+
+        #endregion
+        
+    }
+}
---
+++ TestLists/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestLists")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Willis Towers Watson")]
+[assembly: AssemblyProduct("TestLists")]
+[assembly: AssemblyCopyright("Copyright © Willis Towers Watson 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("53b15b0c-ae8f-41a3-a56c-e156578709cd")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
